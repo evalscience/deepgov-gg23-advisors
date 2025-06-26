2f4b710be08931aa691438f7ad1d468869126a02
Carl Barrdahl <carlbarrdahl@gmail.com>
2025-06-26 23:05:03 +0600

    Fix modelSpecs

--- scripts/review-applications.ts
+++ scripts/review-applications.ts
@@ -223,7 +223,7 @@ async function main() {
 
     const results = await Promise.all(
       validApplications.map((application) =>
-        limit(() => processApplication(application, [modelSpecs[3]]))
+        limit(() => processApplication(application, modelSpecs))
       )
     );
 
